ef8d05b32c5b149578ef520b27f78b38d1f7383f
kevin.li <kevin.li@duethealth.com>
2020-04-18 13:20:17 -0700

    Added TwoSum

---
+++ TwoSum.swift
@@ -0,0 +1,16 @@
+// Kevin Li - 1:13 PM - 4/18/20 - macOS 10.15
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var map: [Int: Int] = [:]
+
+        for (i, num) in nums.enumerated() {
+            if let index = map[num] {
+                return [index, i]
+            }
+            map[target - num] = i
+        }
+
+        fatalError("There must be a solution")
+    }
+}
