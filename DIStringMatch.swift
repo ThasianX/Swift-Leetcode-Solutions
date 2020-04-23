79157dac6330282640d69929f2d239cfb437b664
kevin.li <kevin.li@duethealth.com>
2020-04-22 22:59:53 -0700

    Added DIStringMatch

--- DIStringMatch.swift
+++ DIStringMatch.swift
@@ -1,3 +1,28 @@
 // Kevin Li - 6:22 PM - 4/22/20 - macOS 10.15
 
-import Foundation
+// https://leetcode.com/problems/di-string-match/
+// Time: O(N)
+// Space: O(N)
+
+class Solution {
+    func diStringMatch(_ S: String) -> [Int] {
+        var N = S.count
+        var lo = 0, hi = N
+        var arr = [Int]()
+        arr.reserveCapacity(N + 1)
+
+        for c in S {
+            if c == "I" {
+                arr.append(lo)
+                lo += 1
+            } else {
+                arr.append(hi)
+                hi -= 1
+            }
+        }
+
+        arr.append(lo)
+
+        return arr
+    }
+}
