9a844b771594f2795877c585ac142e1238bf9710
kevin.li <kevin.li@duethealth.com>
2020-04-29 14:37:24 -0700

    Added N-ary tree postorder traversal solution

---
+++ N-aryTreeePostorderTraversal.swift
@@ -0,0 +1,41 @@
+// Kevin Li - 10:50 PM - 4/23/20 - macOS 10.15
+
+// https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+// Time: O(n) - n: number of nodes in root
+// Space: O(n)
+
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
+class Solution {
+
+    func postorder(_ root: Node?) -> [Int] {
+        guard let root = root else {
+            return []
+        }
+        var arr = [Int]()
+        root.postOrder { node in
+            arr.append(node.val)
+        }
+        return arr
+    }
+
+}
+
+extension Node {
+
+    func postOrder(_ visit: (Node) -> Void) {
+        children.forEach { $0.postOrder(visit) }
+        visit(self)
+    }
+
+}
