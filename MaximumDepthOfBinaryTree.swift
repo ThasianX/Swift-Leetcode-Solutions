5f88e33e5b79739f29183c1d311297ad5b43a1b4
kevin.li <kevin.li@duethealth.com>
2020-05-10 21:33:37 -0700

    Added maximum depth of binary tree template

---
+++ MaximumDepthOfBinaryTree.swift
@@ -0,0 +1,28 @@
+// Kevin Li - 7:15 PM - 5/10/20 - macOS 10.15
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// Time: O(
+// Space: O(
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+
+    func maxDepth(_ root: TreeNode?) -> Int {
+
+    }
+
+}
