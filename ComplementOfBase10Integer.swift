7b2daf94120e193e8f6f9d2be415ba6f0d32204e
Kevin Li <kevinli29033@gmail.com>
2020-05-18 20:01:07 -0700

    Fixed error in number complement

---
+++ ComplementOfBase10Integer.swift
@@ -0,0 +1,3 @@
+// Kevin Li - 7:45 PM - 5/18/20
+
+import Foundation
--- NumberComplement.swift
+++ NumberComplement.swift
@@ -8,7 +8,7 @@ class Solution {
 
     func findComplement(_ num: Int) -> Int {
         let binaryRep = String(num, radix: 2)
-        let complement = binaryRep.map { $0 == "0" ? "1" : "0" }
+        let complement = String(binaryRep.map { $0 == "0" ? "1" : "0" })
         return Int(complement, radix: 2)!
     }
 
