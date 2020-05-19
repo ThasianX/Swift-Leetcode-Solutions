a9274927c4c82844a749d547402b3b33f25a35a0
Kevin Li <kevinli29033@gmail.com>
2020-05-18 20:06:08 -0700

    added complement of base 10 integer

--- ComplementOfBase10Integer.swift
+++ ComplementOfBase10Integer.swift
@@ -1,3 +1,24 @@
 // Kevin Li - 7:45 PM - 5/18/20
 
-import Foundation
+// https://leetcode.com/problems/complement-of-base-10-integer/
+// Time: O(n). n: characters in N
+// Space: O(n). We are indirectly storing a string in this process
+
+class Solution {
+
+    func bitwiseComplement(_ N: Int) -> Int {
+        return Int(N.binaryComplement, radix: 2)!
+    }
+
+}
+
+private extension Int {
+
+    var binaryComplement: String {
+        return String(
+            String(self, radix: 2) // original binary
+                .map { ($0 == "0") ? "1" : "0" } // complement binary
+        )
+    }
+
+}
